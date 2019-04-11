8cc04bd901a9e2aec866b92f314152560949844d
chamajiuxi <chamajiuxi@oppo.com>
2019-04-11 15:54:45 +0800

    '添加request-promise超时'

--- utils/getCheerioRequestOpt.js
+++ utils/getCheerioRequestOpt.js
@@ -3,6 +3,7 @@ var cheerio = require('cheerio')
 module.exports = getOptions = (url) => {
   return {
     uri: url,
+    timeout: 5000,
     transform(body) {
       return cheerio.load(body)
     }
